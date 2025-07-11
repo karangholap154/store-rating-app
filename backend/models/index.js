bf2a82fcd36f67d841e46a2e8339174b2a762ebb
karangholap154 <karangholap154@gmail.com>
2025-07-11 14:22:56 +0530

    Switched to DATABASE_URL with SSL for Render

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,31 +1,12 @@
-const dbConfig = require("../config/db.config.js");
 const { Sequelize, DataTypes } = require("sequelize");
+require("dotenv").config();
 
-// ✅ Sequelize with SSL enabled for Render
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.DIALECT,
-  port: dbConfig.PORT,
-  dialectOptions: dbConfig.dialectOptions, // SSL config from db.config.js
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
-
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-
-// Models
-db.user = require("./user.model.js")(sequelize, DataTypes);
-db.store = require("./store.model.js")(sequelize, DataTypes);
-db.rating = require("./rating.model.js")(sequelize, DataTypes);
-
-// Relationships
-db.user.hasMany(db.rating);
-db.rating.belongsTo(db.user);
-
-db.store.hasMany(db.rating);
-db.rating.belongsTo(db.store);
-
-db.user.hasMany(db.store, { foreignKey: 'ownerId' });
-db.store.belongsTo(db.user, { as: 'owner', foreignKey: 'ownerId' });
-
-module.exports = db;
