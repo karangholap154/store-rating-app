63b11976bb735f4c62bf14913d3f55f3abf62f7c
karangholap154 <karangholap154@gmail.com>
2025-07-11 14:25:01 +0530

    Clean .env

--- backend/models/index.js
+++ backend/models/index.js
@@ -10,3 +10,24 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
   },
 });
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Models
+db.user = require("./user.model.js")(sequelize, DataTypes);
+db.store = require("./store.model.js")(sequelize, DataTypes);
+db.rating = require("./rating.model.js")(sequelize, DataTypes);
+
+// Relationships
+db.user.hasMany(db.rating);
+db.rating.belongsTo(db.user);
+
+db.store.hasMany(db.rating);
+db.rating.belongsTo(db.store);
+
+db.user.hasMany(db.store, { foreignKey: 'ownerId' });
+db.store.belongsTo(db.user, { as: 'owner', foreignKey: 'ownerId' });
+
+module.exports = db;
